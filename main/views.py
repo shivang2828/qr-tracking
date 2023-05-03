e663bff83a302639bb7537e9136093a028deab95
SHIVANG GAUR <shivangbabu28.sgg@gmail.com>
2023-05-03 18:33:14 +0530

    FF

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse,redirect
+
+count =0
+
+def home(request):
+    global count
+    return render(request,"home.html", {
+          'count':str(count)
+      })
+
+
+def api(request):
+    global count
+    count +=1
+    return redirect("https://qr-code-tracking.vercel.app/")
